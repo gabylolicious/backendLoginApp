700162f3bcd9d1eb4e99f42e81be611dc654dac2
gabylolicious <gabylolicious@gmail.com>
2026-02-02 16:41:25 -0500

    Se tendría que conectar una BD pero en teoria ya se pueden agregar peticiones con categorias... solo que no funciona

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, Field
 from fastapi.middleware.cors import CORSMiddleware
+from uuid import uuid4
 app = FastAPI()
 
 origins = [
@@ -17,6 +18,19 @@ class LoginRequest(BaseModel):
     username : str = Field(...,min_length=5)
     password : str = Field(...,min_length=8)
 
+class Categoria(BaseModel):
+    id : str | None = None
+    nombre: str
+
+class Videojuego(BaseModel):
+    id : str | None = None
+    nombre : str
+    descripcion : str
+    url_imagen : str
+    categoria : Categoria
+
+categorias = []
+
 @app.post("/login")
 async def login(login_request : LoginRequest):
     if login_request.username == "PROGRAWEB" and login_request.password == "123123123":
@@ -28,4 +42,14 @@ async def login(login_request : LoginRequest):
             status_code=400, 
             detail="Error en login, credenciales incorrectas"
             )
-    
+
+@app.get("/categorias")
+async def list_categorias():
+    return categorias
+
+@app.post("/categoria")
+async def create_categoria(categoria : Categoria):
+    categoria.id = str(uuid4())
+    # TO DO: Acá se conversaría con una base de datos
+    categorias.append(categoria)
+    return categoria

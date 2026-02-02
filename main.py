bdf4e2ab040953c8745f647f740a61e30c64b0df
gabylolicious <gabylolicious@gmail.com>
2026-02-02 16:13:01 -0500

    Primera version de backend para LoginApp, permite validar el login, imprime el error

---
+++ main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, Field
+from fastapi.middleware.cors import CORSMiddleware
+app = FastAPI()
+
+origins = [
+    "*"
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+class LoginRequest(BaseModel):
+    username : str = Field(...,min_length=5)
+    password : str = Field(...,min_length=8)
+
+@app.post("/login")
+async def login(login_request : LoginRequest):
+    if login_request.username == "PROGRAWEB" and login_request.password == "123123123":
+        return{
+            "msg" : "Acceso concedido"
+        }
+    else:
+        raise HTTPException(
+            status_code=400, 
+            detail="Error en login, credenciales incorrectas"
+            )
+    

5691532cca4b5e763b6786a7d2e29fb9360d6650
gabylolicious <gabylolicious@gmail.com>
2026-02-03 15:49:18 -0500

    casi terminado?? CRUD totalmente completo pretty much...

--- main.py
+++ main.py
@@ -45,11 +45,63 @@ async def login(login_request : LoginRequest):
 
 @app.get("/categorias")
 async def list_categorias():
-    return categorias
+    return {
+        "msg" : "",
+        "data" : categorias
+    }
 
-@app.post("/categoria")
+@app.post("/categorias")
 async def create_categoria(categoria : Categoria):
     categoria.id = str(uuid4())
     # TO DO: Acá se conversaría con una base de datos
     categorias.append(categoria)
-    return categoria
+    return {
+        "msg" : "",
+        "data" : categoria
+    } 
+
+@app.put("/categorias")
+async def update_categoria(categoria : Categoria):
+    for cat in categorias:
+        if cat.id == categoria.id:
+            # Si se encuentra la categoría
+            cat.nombre = categoria.nombre
+            return {
+                "msg" : "",
+                "data" : cat
+            }
+    # Busca todos y no lo encuentra
+    raise HTTPException(
+        status_code=404,
+        detail="Categoria id no existe"
+    )
+
+@app.delete("/categorias/{categoria_id}")
+async def delete_categorias(categoria_id : str):
+    for i, cat in enumerate(categorias):
+        # Por cada indice i va a tomar el valor del indice y cat va a tomar el valor de la categoria
+        if cat.id == categoria_id:
+            categorias.pop(i)
+            return {
+                "msg" : ""
+            }
+    # Si recorre el for y nunca encuentra la categoria
+    raise HTTPException(
+        status_code=404,
+        detail="La categoria no se pudo borrar: No encontrada"
+    )
+
+# Que devuelva una categoria por su id (buscar)
+@app.get("/categorias/{categorias_id}")
+async def buscar_categorias(categoria_id : str):
+    for i,cat in enumerate(categorias):
+        if cat.id == categoria_id:
+            return {
+                "msg" : "",
+                "data" : cat
+            }
+    # SI no se encontró tras toda la lista
+    raise HTTPException(
+        status_code=404,
+        detail="La categoria buscada no existe"
+    )

51811c7244939d80dfea2ebcecc094458cddda0f
gabylolicious <gabylolicious@gmail.com>
2026-02-16 16:15:54 -0500

    Relación muchos a muchos

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 import uuid
-from sqlalchemy import UUID, Column, ForeignKey, String, DateTime
+from sqlalchemy import UUID, Column, ForeignKey, String, DateTime, Table
 from sqlalchemy.orm import relationship
 from .database import Base
 
@@ -55,6 +55,13 @@ class CategoriaModel(Base):
 
     videojuegos = relationship("Videojuego", back_populates="categoria")
 
+videojuego_plataforma = Table(
+    "videojuego_plataforma",
+    Base.metadata,
+    Column("videojuego_id",ForeignKey("videojuego.id"), primary_key=True),
+    Column("plataforma_id",ForeignKey("plataforma.id"), primary_key=True)
+)
+
 class Videojuego(Base):
     __tablename__ = "videojuego"
     id = Column (
@@ -72,3 +79,24 @@ class Videojuego(Base):
         ForeignKey("categoria_id", unique=True)
     )
     categoria = relationship("CategoriaModel", back_populates="videojuegos")
+    plataformas = relationship (
+        "Plataforma",
+        secondary=videojuego_plataforma,
+        back_populates="videojuegos"
+    )
+
+class Plataforma(Base):
+    __tablename__ = "plataforma"
+    id = Column (
+        UUID(as_uuid = True),
+        primary_key = True,
+        default=lambda: str(uuid.uuid4()),
+        index=True
+    )
+    nombre = Column(String)
+
+    videojuegos = relationship (
+        "Videojuego",
+        secondary=videojuego_plataforma,
+        back_populates="plataformas"
+    )
